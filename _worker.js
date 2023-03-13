8f97a03c8c09ada9acf31680243b19967be165fe
heku66 <102395422+heku66@users.noreply.github.com>
2023-03-13 17:31:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="gotify-prod-freenoom-5hw0ha.mo6.mogenius.io";
+        url.hostname="chat-prod-freenoom-5hw0ha.mo6.mogenius.io";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
